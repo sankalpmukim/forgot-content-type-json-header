8b6521183bca956c108a20ffc834636b2962fb66
Sankalp Mukim <SANKALP.MUKIM@ibm.com>
2025-07-25 17:05:23 +0530

    actual functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,93 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let diagnosticCollection: vscode.DiagnosticCollection;
+
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "forgot-content-type-json-header" is now active!'
-  );
+  diagnosticCollection =
+    vscode.languages.createDiagnosticCollection("fetch-content-type");
+  context.subscriptions.push(diagnosticCollection);
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "forgot-content-type-json-header.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from Forgot Content Type JSON Header!"
-      );
+  // Check open document on activation
+  if (vscode.window.activeTextEditor) {
+    checkDocument(vscode.window.activeTextEditor.document);
+  }
+
+  // Check when document changes
+  vscode.workspace.onDidChangeTextDocument((event) => {
+    checkDocument(event.document);
+  });
+
+  // Check when switching between documents
+  vscode.window.onDidChangeActiveTextEditor((editor) => {
+    if (editor) {
+      checkDocument(editor.document);
     }
-  );
-
-  context.subscriptions.push(disposable);
+  });
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function checkDocument(document: vscode.TextDocument) {
+  // Only check JavaScript/TypeScript files
+  const supportedLanguages = [
+    "javascript",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+  ];
+  if (!supportedLanguages.includes(document.languageId)) {
+    return;
+  }
+
+  const diagnostics: vscode.Diagnostic[] = [];
+  const text = document.getText();
+
+  // Find all fetch calls
+  const fetchRegex = /fetch\s*\(\s*[^,]+\s*,\s*\{([^}]+)\}/g;
+  let match;
+
+  while ((match = fetchRegex.exec(text)) !== null) {
+    const fetchCallContent = match[1];
+    const hasJsonStringify = /JSON\.stringify\s*\(/.test(fetchCallContent);
+
+    if (hasJsonStringify) {
+      // Check if Content-Type is set to application/json
+      const hasCorrectContentType =
+        /['"`]content-type['"`]\s*:\s*['"`]application\/json['"`]/i.test(
+          fetchCallContent
+        );
+
+      if (!hasCorrectContentType) {
+        // Find the position of JSON.stringify in the document
+        const jsonStringifyMatch = /JSON\.stringify\s*\(/.exec(
+          fetchCallContent
+        );
+        if (jsonStringifyMatch) {
+          const absoluteIndex =
+            match.index +
+            match[0].indexOf(fetchCallContent) +
+            jsonStringifyMatch.index;
+          const position = document.positionAt(absoluteIndex);
+          const endPosition = document.positionAt(
+            absoluteIndex + jsonStringifyMatch[0].length - 1
+          );
+
+          const diagnostic = new vscode.Diagnostic(
+            new vscode.Range(position, endPosition),
+            'Using JSON.stringify() without "Content-Type": "application/json" header',
+            vscode.DiagnosticSeverity.Warning
+          );
+
+          diagnostic.source = "fetch-content-type-checker";
+          diagnostics.push(diagnostic);
+        }
+      }
+    }
+  }
+
+  diagnosticCollection.set(document.uri, diagnostics);
+}
+
+export function deactivate() {
+  if (diagnosticCollection) {
+    diagnosticCollection.dispose();
+  }
+}
