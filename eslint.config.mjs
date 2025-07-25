2ef4ee4b94b61e38fbd02eb7a0bbb48015f84b9d
Sankalp Mukim <SANKALP.MUKIM@ibm.com>
2025-07-25 16:42:35 +0530

    initial works

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    files: ["**/*.ts"],
+  },
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"],
+        },
+      ],
+
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
+      semi: "warn",
+    },
+  },
+];
